1eb5a12d5fbc01488c784d420f54442838e6ea77
sinedied <noda@free.fr>
2022-08-23 12:14:13 +0200

    feat: implement workshop loading

--- website/src/app/frontmatter.ts
+++ website/src/app/frontmatter.ts
@@ -2,8 +2,20 @@ import { parse } from 'yaml';
 
 const frontMatterRegex = /^(?:---\r?\n([\s\S]+?)\r?\n---\r?\n)?([\s\S]*?)$/;
 
+export type FrontMatterData = Partial<{
+  description: string;
+  authors: string;
+  banner_url: string;
+  video_url: string;
+  duration_minutes: string;
+  published: string;
+  tags: string;
+  wt_id: string;
+  oc_id: string;
+}>;
+
 export interface FrontMatterParseResult {
-  data: Record<string, any>;
+  data: FrontMatterData;
   content: string;
 }
 
---
+++ website/src/app/typings.d.ts
@@ -0,0 +1,3 @@
+declare module 'cxa-track/tracking' {
+  function updateTrackingCodeInText(text: string, trackingCode: string, locale?: boolean|string, extraParams?: Record<string, string>): string;
+}
