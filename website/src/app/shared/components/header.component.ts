1fc074056b3e6f9c04541fe657383694dee8332d
sinedied <noda@free.fr>
2022-09-05 16:03:09 +0200

    refactor: move styles

--- website/src/app/shared/components/header.component.ts
+++ website/src/app/shared/components/header.component.ts
@@ -22,6 +22,36 @@ import { Link } from '../link';
   styles: [`
     @import '../../../variables';
 
+    .navbar {
+      position: sticky;
+      z-index: 10;
+      top: 0;
+      background: var(--primary);
+      padding: var(--space-xs);
+      height: var(--navbar-height);
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.25);
+      display: flex;
+      align-items: center;
+      padding: var(--space-xs) var(--space-md);
+
+      .title {
+        color: var(--text-light);
+        font-size: 1.5rem;
+        font-weight: 500;
+        line-height: 1.5;
+      }
+
+      button {
+        border: 0;
+        color: var(--text-light);
+        transition: opacity var(--transition-duration);
+
+        &:hover {
+          opacity: 0.7;
+        }
+      }
+    }
+
     .menu-toggle {
       margin-left: calc(-1 * var(--space-md));
     }
