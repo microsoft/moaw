6b64a2bc4e2fcb1cbf9e41d005cef78386c58d9a
sinedied <noda@free.fr>
2022-10-04 18:11:07 +0200

    fix: url on same origin

--- website/src/app/catalog/content-entry.ts
+++ website/src/app/catalog/content-entry.ts
@@ -39,7 +39,9 @@ export async function loadCatalog(): Promise<ContentEntry[]> {
 }
 
 function createWorkshopUrl(file: string) {
-  if (file.startsWith('http')) {
+  if (file.startsWith(window.location.origin)) {
+    return file.replace(window.location.origin, '');
+  } else if (file.startsWith('http')) {
     return file;
   }
   return `${window.location.origin}${getBasePath()}workshop/${file}`;
