ef8b5c4ab75c7e4281b618c8983daced866e0f46
Ross Boast <33289376+rossboast@users.noreply.github.com>
2026-01-22 09:58:29 +0000

    feat: parameterize base href for deployment and update related scripts (#173)
    Co-authored-by: Yohan Lasorsa <noda@free.fr>

--- packages/website/scripts/update-env.sh
+++ packages/website/scripts/update-env.sh
@@ -16,8 +16,12 @@ fi
 dist_folder="dist/website"
 version="sha.$(git rev-parse --short HEAD)"
 ai_search_url="${AI_SEARCH_URL:-}"
+base_href="${BASE_HREF:-/}"
 
 perl -i -pe "s/__VERSION__/$version/g" $dist_folder/main.*.js
 perl -i -pe "s/__AI_SEARCH_URL__/$ai_search_url/g" $dist_folder/main.*.js
+perl -i -pe "s|__BASE_HREF__|$base_href|g" $dist_folder/404.html
+perl -i -pe "s|<base href=\"/\">|<base href=\"$base_href\">|g" $dist_folder/index.html
 
 echo Version: $version
+echo Base href: $base_href
